0798a62942e39ca234311ce38ef62fcef587061e
Paramveer Kumar Singh <paramveer.22scse1010256@galgotiasuniversity.edu.in>
2025-06-23 18:17:37 +0530

    Time: 2 ms (38.69%), Space: 44.6 MB (63.78%) - LeetHub

---
+++ 0046-permutations/0046-permutations.java
@@ -0,0 +1,23 @@
+class Solution {
+    public static void per(int[]nums , List<Integer> ds , List<List<Integer>> ans , boolean[] freq){
+        if(ds.size()==nums.length){
+            ans.add(new ArrayList<>(ds));
+            return;
+        }
+        for(int i=0;i<nums.length;i++){
+            if(! freq[i]){
+                freq[i]=true;
+                ds.add(nums[i]);
+                per(nums,ds,ans,freq);
+                ds.remove(ds.size()-1);
+                freq[i]=false;
+            }
+        }
+    }
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans=new ArrayList<>();
+        boolean [] freq=new boolean[nums.length];
+        per(nums,new ArrayList<>(),ans,freq);
+        return ans;
+    }
+}
