46f40060b649477489cfc0db4d8cee9f4408b071
Paramveer Kumar Singh <paramveer.22scse1010256@galgotiasuniversity.edu.in>
2025-06-24 02:41:26 +0530

    Time: 2 ms (86.04%), Space: 44.7 MB (88.39%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.java
@@ -0,0 +1,47 @@
+class Solution {
+      public static void nqueen(char[][] board, int row, int n, List<List<String>> res) {
+        if (row == n) {
+            res.add(construct(board));
+            return;
+        }
+
+        for (int col = 0; col < n; col++) {
+            if (isSafe(board, row, col, n)) {
+                board[row][col] = 'Q';
+                nqueen(board, row + 1, n, res);
+                board[row][col] = '.'; // backtrack
+            }
+        }
+    }
+
+    public static boolean isSafe(char[][] board, int row, int col, int n) {
+        for (int i = 0; i < row; i++) {
+            if (board[i][col] == 'Q') return false;
+        }
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') return false;
+        }
+        for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
+            if (board[i][j] == 'Q') return false;
+        }
+        return true;
+    }
+
+    public static List<String> construct(char[][] board) {
+        List<String> result = new ArrayList<>();
+        for (char[] row : board) {
+            result.add(new String(row));
+        }
+        return result;
+    }
+
+    public static List<List<String>> solveNQueens(int n) {
+        List<List<String>> res = new ArrayList<>();
+        char[][] board = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(board[i], '.');
+        }
+        nqueen(board, 0, n, res);
+        return res;
+    }
+}
