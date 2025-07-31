d50057da2ae498f58f6b4d1617acc22a755d9e5a
Paramveer Kumar Singh <paramveer.22scse1010256@galgotiasuniversity.edu.in>
2025-07-31 17:18:23 +0530

    Time: 13 ms (18.93%), Space: 45.4 MB (85.83%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,9 +1,14 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int value=0;
-        for(int i=0;i<nums.length;i++){
-            value=value^nums[i];
+        HashMap<Integer,Integer> mp=new HashMap<>();
+        for (int i=0;i<nums.length;i++){
+            mp.put(nums[i], mp.getOrDefault(nums[i],0)+1);
         }
-        return value;
+        for (int i=0;i<nums.length;i++){
+            if (mp.get(nums[i])==1){
+             return nums[i];
+           }
+        }
+        return -1;
     }
 }
