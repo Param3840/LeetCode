c6c1861a10b81bfa7c882e67ea412f47dfbab06c
Paramveer Kumar Singh <paramveer.22scse1010256@galgotiasuniversity.edu.in>
2025-08-03 16:27:23 +0530

    Time: 1 ms (62.94%), Space: 41.3 MB (95.73%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -3,7 +3,7 @@ class Solution {
         if(strs==null || strs.length==0){
             return "";
         }
-        for(int i=0;i<strs[0].length;i++){
+        for(int i=0;i<strs[0].length();i++){
             char current=strs[0].charAt(i);
             for(int j=1;j<strs.length;j++){
                 if(i>=strs[j].length() || strs[j].charAt(i) != current){
