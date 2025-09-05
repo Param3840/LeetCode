d1765734275a142bb9567f29315bc18fbd9c6354
Paramveer Kumar Singh <paramveer.22scse1010256@galgotiasuniversity.edu.in>
2025-09-05 12:14:54 +0530

    Time: 0 ms (100%), Space: 43.3 MB (67.46%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -1,34 +1,32 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-           int st = 0, end = nums.length - 1;
+        int n = nums.length;
+        int st = 0;
+        int end = n - 1;
 
-                   while (st <= end) {
-                               int mid = st + (end - st) / 2;
+        while (st <= end) {
+            int mid = st + (end - st) / 2;
+            if (target == nums[mid])
+                return true;
 
-                                           if (nums[mid] == target) return true;
-
-                                                       // If right half is sorted
-                                                                   if (nums[mid] < nums[end]) {
-                                                                                   if (target > nums[mid] && target <= nums[end]) {
-                                                                                                       st = mid + 1;
-                                                                                                                       } else {
-                                                                                                                                           end = mid - 1;
-                                                                                                                                                           }
-                                                                                                                                                                       }
-                                                                                                                                                                                   // If left half is sorted
-                                                                                                                                                                                               else if (nums[mid] > nums[end]) {
-                                                                                                                                                                                                               if (target >= nums[st] && target < nums[mid]) {
-                                                                                                                                                                                                                                   end = mid - 1;
-                                                                                                                                                                                                                                                   } else {
-                                                                                                                                                                                                                                                                       st = mid + 1;
-                                                                                                                                                                                                                                                                                       }
-                                                                                                                                                                                                                                                                                                   }
-                                                                                                                                                                                                                                                                                                               // nums[mid] == nums[end], duplicates: shrink search space
-                                                                                                                                                                                                                                                                                                                           else {
-                                                                                                                                                                                                                                                                                                                                           end--;
-                                                                                                                                                                                                                                                                                                                                                       }
-                                                                                                                                                                                                                                                                                                                                                               }
-
-                                                                                                                                                                                                                                                                                                                                                                       return false;
+            if (nums[st] == nums[mid] && nums[end] == nums[mid]) {
+                st++;
+                end--;
+                //                continue;
+            } else if (nums[mid] <= nums[end]) {
+                if (target > nums[mid] && target <= nums[end]) {
+                    st = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            } else {
+                if (target >= nums[st] && target < nums[mid]) {
+                    end = mid - 1;
+                } else {
+                    st = mid + 1;
+                }
+            }
+        }
+        return false;
     }
 }
