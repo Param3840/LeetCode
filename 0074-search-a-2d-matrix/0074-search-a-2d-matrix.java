73a99cd1d48a8a22785f101de808b0d85e5f56aa
Paramveer Kumar Singh <paramveer.22scse1010256@galgotiasuniversity.edu.in>
2025-09-15 03:37:59 +0530

    Time: 0 ms (100%), Space: 42.3 MB (50.34%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,11 +1,16 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[i].length;j++){
-                if(matrix[i][j]==target){
-                    return true;
-                }
-            }
+        int n = matrix.length, m = matrix[0].length;
+        int low = 0, high = n * m - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int row = mid / m, col = mid % m;
+            if (matrix[row][col] == target)
+                return true;
+            else if (matrix[row][col] < target)
+                low = mid + 1;
+            else
+                high = mid - 1;
         }
         return false;
     }
